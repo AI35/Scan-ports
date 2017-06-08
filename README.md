d26e67d971cc4456437650f5f28e7f6f20fd7633
ALI B OTHMAN <alosh.othman55@gmail.com>
2017-06-08 15:47:18 +0300

    +Support Python 3
    Now you can open Scripts on Python 2.7.x or 3.6.x

--- README.md
+++ README.md
@@ -2,19 +2,19 @@
 ###### This program is programmed to check ports for any host you want.
 
 [![Scan ports](https://img.shields.io/badge/build-passing-brightgreen.svg)]()
-[![version](https://img.shields.io/badge/version-1.0.5-green.svg)]()
+[![version](https://img.shields.io/badge/version-1.1.5-green.svg)]()
 [![status](https://img.shields.io/badge/status-stable-brightgreen.svg)]()
-[![python](https://img.shields.io/badge/python-2.7-blue.svg)](http://www.python.org/download/)
+[![python](https://img.shields.io/badge/python-2.7.x , 3.6.x-blue.svg)](http://www.python.org/download/)
 [![windows](https://img.shields.io/badge/windows-tested-brightgreen.svg)]()
 [![linux](https://img.shields.io/badge/linux-Not%20woking-lightgrey.svg)]()
 [![license](https://img.shields.io/npm/l/express.svg)](https://github.com/LinePY/Scan-ports/blob/master/LICENSE)
 
 ## REQUIREMENTS
-#### Python-2.7.x --> http://www.python.org/download/
+#### Only Python-3.6.x or 2.7.x --> http://www.python.org/download/
 
 ## Notes
 #### 1) Not Working on Linux.
-#### 2) Not working on python 3 (you want change some codes to working on python 3).
+#### 2) Scan_Ports py2.7.x(for python 2.7) , Scan_Ports py3.6.x(for python 3.6)
 #### 3) You can scan your computer by Enter host : 127.0.0.1 (or) localhost .
 
 ## HOW TO RUN 
@@ -31,11 +31,15 @@
 #### 4) If port 445 is open This means that the host is at risk from the WannaCry Ransomware virus.
 
 ## LICENSE
+'''
+Copyright 2017 LinePY, Inc.
 
-#### Copyright 2017 LinePY, Inc.
+Scan Ports
 
-##### Licensed under the MIT License , you may not use this file except in compliance with the License. You may obtain a copy of the License at 
+   Licensed under the MIT License , you may not use this
+   file except in compliance with the License.
+   You may obtain a copy of the License at :
 
-##### https://github.com/LinePY/Scan-ports/blob/master/LICENSE
-
-###### ALI .B .OTH - ORG : LinePY ©
+   https://github.com/LinePY/Scan-ports/blob/master/LICENSE
+'''
+###### ALI .B .OTH - ORG : LinePY ï¿½
---
+++ Scan_ports py3.6.x.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+# SCAN PORTS
+# DEV BY : ALI .B .OTH
+# My ORG : LinePY ©
+# Github account : https://github.com/alosh55
+# My ORG on Github : https://github.com/orgs/LinePY
+# version : 1.1.5
+
+import socket
+import sys, platform
+from time import sleep
+from multiprocessing import cpu_count
+
+
+def compinfo():
+    print('\nScan Ports v1.1.5')
+    np = cpu_count()
+    print('\nYou have {0:0} CPUs'.format(np))
+
+    print()
+    print('system   :', platform.system())
+    print('node     :', platform.node())
+    print('release  :', platform.release())
+    print('version  :', platform.version())
+    print('machine  :', platform.machine())
+    print('processor:', platform.processor())
+    print('\nDEV BY : ALI .B .OTH - ORG : LinePY ©')
+    for n in range(1, 6):
+        sleep(0.5)
+        print('.',end=' ')
+
+
+def starting():
+    print('\nSTARTING ', end=' ')
+    for i in range(1, 6):
+        sleep(1)
+        print('.',end=' ')
+    print()
+
+
+if __name__ == "__main__":
+    compinfo()
+    if platform.system() != 'Windows':
+        print('\nNot Working on : ', platform.system())
+    else:
+        if platform.python_version() < '3.6':
+            print('\nYour python version is ', platform.python_version())
+            print('\nPlease install python 3.6.x or later')
+            print('\nEXITING ',end=' ')
+            for e in range(1, 11):
+                sleep(1)
+                print('.',end=' ')
+        else:
+            check = True
+            while check:
+                host = input("\n\nEnter Host : ")
+                try:
+                    host_ip = socket.gethostbyname(host)
+                except:
+                    print("Invaild Host , Please Enter vaild Host...")
+                else:
+                    check = False
+            Q1 = True
+            while Q1:
+                Eport = input(
+                    "\n-Do you want enter min and max port ? (Y for yes , N for no)\n-if you choose no => (min port=1 , max port = 5000)\n-")
+                if Eport == "Y" or Eport == "y" or Eport == "N" or Eport == "n":
+                    Q1 = False
+                else:
+                    print("\nWrong choice..Try again..")
+            if Eport == "Y" or Eport == "y":
+                cp = True
+                while cp:
+                    try:
+                        min_port = int(input("\nEnter min port: "))
+                        max_port = int(input("\nEnter max port: "))
+                    except:
+                        print("error..!! Please Try again..")
+                    else:
+                        cp = False
+            else:
+                cp = False
+                min_port = 1
+                max_port = 5000
+            starting()
+
+            print("-" * 65)
+            print("Please wait.... Scanning this Host : ", host_ip, '(', host, ')')
+            print("-" * 65)
+            try:
+                for port in range(min_port, max_port):
+                    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                    sock.settimeout(0.5)
+
+                    result = sock.connect_ex((host_ip, port))
+                    if result == 0:
+                        print("Port %d : \t Open " % (port))
+                        sock.close()
+            except KeyboardInterrupt:
+                print("You pressed Ctrl+C")
+                print("\n\n[*] User Requested An Interrupt.")
+                print("[*] Application Shutting Down.")
+                sleep(5)
+                sys.exit()
+            except socket.gaierror:
+                print('Hostname could not be resolved. Exiting..')
+                sleep(5)
+                sys.exit()
+            except socket.error:
+                print("Couldn't connect to server")
+                sleep(5)
+                sys.exit()
+
+            print("\n[*] Finish.. ")
+            print('\nDEV BY : ALI .B .OTH - ORG : LinePY ©\nGithub account : https://github.com/alosh55\nMy ORG on Github : https://github.com/orgs/LinePY')
+            while True:
+                pass
